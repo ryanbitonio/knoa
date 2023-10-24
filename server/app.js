b407c87c0948a9203b4025622ce688ea9887ba83
Ryan G. Bitonio <ryanbitonio.ayang@gmail.com>
2023-10-25 00:23:58 +0800

    Implementing cors

--- server/app.js
+++ server/app.js
@@ -1,7 +1,15 @@
 import express from "express";
 import cardsRouter from "./routes/cards.router.js";
+import cors from "cors";
 
 const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 app.use((req, res, next) => {
   const start = Date.now();
