08b498a424825a6d871acc8b6d811996ec1cbb5e
Ryan G. Bitonio <ryanbitonio.ayang@gmail.com>
2023-11-19 14:11:40 +0800

    Updating the images in slideshow

--- client/src/data/slideshow.js
+++ client/src/data/slideshow.js
@@ -1,9 +1,11 @@
-import welcomeImage from "../assets/welcome-img.jpg";
-import scheduleImage from "../assets/schedule-img.jpg";
+import image1 from "../assets/image-1.jpg";
+import image2 from "../assets/image-2.jpg";
+import image3 from "../assets/image-3.jpg";
+import image4 from "../assets/image-4.jpg";
 
 export const carousel = [
-  { animate: "animate-[xfade_16s_12s_infinite]", image: scheduleImage },
-  { animate: "animate-[xfade_16s_8s_infinite]", image: welcomeImage },
-  { animate: "animate-[xfade_16s_4s_infinite]", image: scheduleImage },
-  { animate: "animate-[xfade_16s_0s_infinite]", image: welcomeImage },
+  { animate: "animate-[xfade_16s_12s_infinite]", image: image4 },
+  { animate: "animate-[xfade_16s_8s_infinite]", image: image3 },
+  { animate: "animate-[xfade_16s_4s_infinite]", image: image2 },
+  { animate: "animate-[xfade_16s_0s_infinite]", image: image1 },
 ];
