abbe2041d965c2bf7c9678c70dde5984b2b67f17
Ryan G. Bitonio <ryanbitonio.ayang@gmail.com>
2023-12-03 02:25:55 +0800

    Fixing the responsiveness of toast in RSVP

--- client/src/components/layout/Rsvp.jsx
+++ client/src/components/layout/Rsvp.jsx
@@ -86,91 +86,93 @@ const Rsvp = () => {
   }
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-slate-50">
-      <Form {...form}>
-        <form onSubmit={handleSubmit(onSubmit)} autoComplete="off">
-          <Card>
-            <CardHeader>
-              <CardTitle>RSVP</CardTitle>
-              <CardDescription>
-                Dedication & birthday celebration
-              </CardDescription>
-            </CardHeader>
-            <CardContent>
-              {fields.map(({ name, formLabel, placeholder }) => (
+    <>
+      <div className="flex flex-col items-center justify-center h-screen bg-slate-50">
+        <Form {...form}>
+          <form onSubmit={handleSubmit(onSubmit)} autoComplete="off">
+            <Card>
+              <CardHeader>
+                <CardTitle>RSVP</CardTitle>
+                <CardDescription>
+                  Dedication & birthday celebration
+                </CardDescription>
+              </CardHeader>
+              <CardContent>
+                {fields.map(({ name, formLabel, placeholder }) => (
+                  <FormField
+                    control={control}
+                    name={name}
+                    key={name}
+                    render={({ field }) => (
+                      <FormItem className="mb-2 ">
+                        <FormLabel>{formLabel}</FormLabel>
+                        <FormControl>
+                          <Input placeholder={placeholder} {...field} />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                ))}
+                <p className="leading-7 [&:not(:first-child)]:mt-4 mb-4 text-sm">
+                  Will you be able to join us at our son’s dedication?
+                </p>
+
                 <FormField
                   control={control}
-                  name={name}
-                  key={name}
+                  name="response"
                   render={({ field }) => (
-                    <FormItem className="mb-2 ">
-                      <FormLabel>{formLabel}</FormLabel>
+                    <FormItem>
                       <FormControl>
-                        <Input placeholder={placeholder} {...field} />
+                        <RadioGroup
+                          tabIndex={-1}
+                          onValueChange={field.onChange}
+                          defaultValue={field.value}
+                        >
+                          {response.map(({ value, label }) => (
+                            <div
+                              key={value}
+                              tabIndex={-1}
+                              className="border border-solid rounded-lg border-slate-200 focus:rounded-lg focus:outline-none focus:ring-2 focus:ring-slate-950 focus:ring-offset-2 "
+                            >
+                              <FormItem className="flex space-y-0 cursor-pointer">
+                                <FormControl>
+                                  <RadioGroupItem
+                                    value={value}
+                                    className="hidden"
+                                  />
+                                </FormControl>
+                                <FormLabel className="w-full py-2 text-sm font-normal text-center cursor-pointer ">
+                                  {label}
+                                </FormLabel>
+                              </FormItem>
+                            </div>
+                          ))}
+                        </RadioGroup>
                       </FormControl>
                       <FormMessage />
                     </FormItem>
                   )}
                 />
-              ))}
-              <p className="leading-7 [&:not(:first-child)]:mt-4 mb-4 text-sm">
-                Will you be able to join us at our son’s dedication?
-              </p>
-
-              <FormField
-                control={control}
-                name="response"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormControl>
-                      <RadioGroup
-                        tabIndex={-1}
-                        onValueChange={field.onChange}
-                        defaultValue={field.value}
-                      >
-                        {response.map(({ value, label }) => (
-                          <div
-                            key={value}
-                            tabIndex={-1}
-                            className="border border-solid rounded-lg border-slate-200 focus:rounded-lg focus:outline-none focus:ring-2 focus:ring-slate-950 focus:ring-offset-2 "
-                          >
-                            <FormItem className="flex space-y-0 cursor-pointer">
-                              <FormControl>
-                                <RadioGroupItem
-                                  value={value}
-                                  className="hidden"
-                                />
-                              </FormControl>
-                              <FormLabel className="w-full py-2 text-sm font-normal text-center cursor-pointer ">
-                                {label}
-                              </FormLabel>
-                            </FormItem>
-                          </div>
-                        ))}
-                      </RadioGroup>
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-            </CardContent>
-            <CardFooter className="flex justify-between">
-              <Link tabIndex={-1} to="/">
-                <Button variant="outline">Cancel</Button>
-              </Link>
-              <Button disabled={isSubmitting} type="submit">
-                {isSubmitting ? (
-                  <Loader2 className="w-4 h-4 mr-2 animate-spin" />
-                ) : (
-                  "Submit"
-                )}
-              </Button>
-            </CardFooter>
-          </Card>
-        </form>
-      </Form>
+              </CardContent>
+              <CardFooter className="flex justify-between">
+                <Link tabIndex={-1} to="/">
+                  <Button variant="outline">Cancel</Button>
+                </Link>
+                <Button disabled={isSubmitting} type="submit">
+                  {isSubmitting ? (
+                    <Loader2 className="w-4 h-4 mr-2 animate-spin" />
+                  ) : (
+                    "Submit"
+                  )}
+                </Button>
+              </CardFooter>
+            </Card>
+          </form>
+        </Form>
+      </div>
       <Toaster />
-    </div>
+    </>
   );
 };
 
